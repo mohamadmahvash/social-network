4ede49c5cfdf3a08c698948df066417f825e4b16
mahvash <mohammadmahvash1@gmail.com>
2026-06-05 20:44:52 +0330

    class based view

--- home/views.py
+++ home/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.views import View
 
-def home(request):
-    return render(request,'home/index.html')
+
+class HomeView(View):
+    def get(self, request):
+        return render(request, "home/index.html")
