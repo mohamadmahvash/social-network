b34418e929b2d446ccfe58d872bb372f96ca1935
mahvash <mohammadmahvash1@gmail.com>
2026-06-07 18:07:18 +0330

    user registration

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = 'account'
 urlpatterns = [
-    path('register/' , views.UserRegisterView.as_view() , name = 'register'),
+    path('register/', views.UserRegisterView.as_view(), name='register'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .forms import UserRegisterForm
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 class UserRegisterView(View):
+    form_class = UserRegisterForm
+    template_name = 'registration/register.html'
+
     def get(self, request):
-        form = UserRegisterForm()
-        return render(request, 'account/user_register.html', {'form': form})
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
 
     def post(self, request):
-        pass
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(username=cd['username'], email=cd['email'], password=cd['password'])
+            messages.success(request, 'Registered Successfully.', extra_tags='success')
+            return redirect('home:home')
+        return render(request, self.template_name, {'form': form})
