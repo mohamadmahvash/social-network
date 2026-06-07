1b4bbc04f3dad3d6db1eeeddb8d53eb3d429dbdc
mahvash <mohammadmahvash1@gmail.com>
2026-06-07 16:02:45 +0330

    registration

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,12 @@
 from django import forms
 
+
 class UserRegisterForm(forms.Form):
-    username = forms.CharField()
-    email = forms.EmailField()
-    password = forms.CharField()
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'Your name', 'class': "form-control"}))
+
+    email = forms.EmailField(widget=forms.EmailInput(
+        attrs={'placeholder': 'Your email', 'class': "form-control"}))
+
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={'placeholder': 'Your password', 'class': "form-control"}))
