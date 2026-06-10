787f2fe6069bba35fa4520e0e487195dd8d2cec2
mahvash <mohammadmahvash1@gmail.com>
2026-06-10 19:06:15 +0330

    create login and logout form

--- account/forms.py
+++ account/forms.py
@@ -3,6 +3,14 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 
 
+class UserLoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'Your name', 'class': "form-control"}))
+
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={'placeholder': 'Your password', 'class': "form-control"}))
+
+
 class UserRegisterForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(
         attrs={'placeholder': 'Your name', 'class': "form-control"}))
--- account/views.py
+++ account/views.py
@@ -1,8 +1,37 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserLoginForm
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+
+
+class UserLoginView(View):
+    form_class = UserLoginForm
+    template_name = 'account/User_login.html'
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(username=cd['username'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'You are now logged in', extra_tags='success')
+                return redirect('home:home')
+            messages.warning(request, 'username/password is wrong', extra_tags='warning')
+        return render(request, self.template_name, {'form': form})
+
+
+class UserLogoutView(View):
+    def get(self, request):
+        logout(request)
+        messages.success(request, 'You are now logged out', extra_tags='success')
+        return redirect('home:home')
 
 
 class UserRegisterView(View):
