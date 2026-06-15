7748029984b5f6654b0b43c45b9390dacd46f6ad
mahvash <mohammadmahvash1@gmail.com>
2026-06-15 18:50:16 +0330

    create post model

--- home/models.py
+++ home/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     slug = models.SlugField()
-    created = models.DateTimeField()
-    updated = models.DateTimeField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.slug} - {self.created}'
