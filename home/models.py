ed662d28de69fb3756d07ee8e66345cfeb757a41
mahvash <mohammadmahvash1@gmail.com>
2026-06-25 09:40:58 +0330

    get_absolute_url

--- home/models.py
+++ home/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+
 
 class Post(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -10,3 +12,6 @@ class Post(models.Model):
 
     def __str__(self):
         return f'{self.slug} - created at:{self.created}'
+
+    def get_absolute_url(self):
+        return reverse('home:detail', args=(self.id, self.slug))
